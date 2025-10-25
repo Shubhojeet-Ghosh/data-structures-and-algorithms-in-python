5c62ee9d3a5b91fe1d7104f3ecca45260774c295
Shubhojeet Ghosh <shubhojeet.official@gmail.com>
2025-10-25 17:18:06 +0530

    Add implementation for groupAnagrams algorithm

---
+++ 02_arrays_and_hashing/group_anagrams.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sorted_strs = []
+        sorted_str_hash = {}
+
+        for str in strs:
+            # print(str)
+
+            sorted_str = sorted(str)
+            sorted_str = "".join(sorted_str)
+            
+            if(sorted_str in sorted_str_hash):
+                sorted_str_hash[sorted_str].append(str)
+            else:
+                sorted_str_hash[sorted_str] = [str]
+
+        final_list = []
+        for key,value in sorted_str_hash.items():
+            final_list.append(value)
+
+        return final_list
+
+
+solution = Solution()
+print(solution.groupAnagrams( ["a"]))
